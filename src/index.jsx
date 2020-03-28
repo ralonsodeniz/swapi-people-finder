8607fb0e4c12ee3ed1bb0b3fe472bd8eaa9f2296
ralonsodeniz <ralonsodeniz@gmail.com>
2020-03-28 17:02:52 +0100

    Added redux persist for savedArray. Fixed a bug in full screen mode mobile that cancelled the scrolling

--- src/index.jsx
+++ src/index.jsx
@@ -2,23 +2,26 @@ import React from 'react';
 import ReactDom from 'react-dom';
 import { Provider } from 'react-redux';
 import { ThemeProvider } from 'styled-components';
+import { PersistGate } from 'redux-persist/integration/react';
 
-import store from './redux/store';
+import { store, persistor } from './redux/store';
 import theme from './style/theme';
 
-// import serviceWorker from './serviceWorker';
+import serviceWorker from './serviceWorker';
 
 import App from './components/App/App';
 import AppHelmet from './Helmet';
 
 ReactDom.render(
   <Provider store={store}>
-    <ThemeProvider theme={theme}>
-      <AppHelmet />
-      <App />
-    </ThemeProvider>
+    <PersistGate persistor={persistor}>
+      <ThemeProvider theme={theme}>
+        <AppHelmet />
+        <App />
+      </ThemeProvider>
+    </PersistGate>
   </Provider>,
   document.getElementById('root')
 );
 
-// serviceWorker();
+serviceWorker();
